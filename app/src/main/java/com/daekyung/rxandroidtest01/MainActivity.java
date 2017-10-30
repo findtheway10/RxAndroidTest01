c3952451a08af8e85bf7c5025d3b2b76afeb0493
dk kim <findtheway2589@gmail.com>
2017-10-30 14:48:54 +0900

    commit

--- app/src/main/java/com/daekyung/rxandroidtest01/MainActivity.java
+++ app/src/main/java/com/daekyung/rxandroidtest01/MainActivity.java
@@ -1,13 +1,46 @@
 package com.daekyung.rxandroidtest01;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.observers.DisposableObserver;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.activity_main_text_view);
+
+        Observer<String> observer = new DisposableObserver<String>() {
+            @Override
+            public void onNext(String s) {
+                textView.setText(s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+
+        Observable.<String>create(s -> {
+
+            s.onNext("Hello world");
+            s.onComplete();
+
+        }).subscribe(observer);
     }
 }
